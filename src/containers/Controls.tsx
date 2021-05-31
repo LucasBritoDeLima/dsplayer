60e0b66d1bce93d5ecc6995c4410b1cebc614d33
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2021-05-30 22:45:00 -0300

    Projeto Criado

---
+++ src/containers/Controls.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import cover from '../assets/images/cover/cradles.jpg';
+import ControlButton from './components/ControlButton';
+import { stop, play, prev, next, pause } from "../assets/images/buttons";
+
+const Controls = () => {
+  const [isPaused, setIsPaused] = useState(true);
+
+  const handlePlay = () => {
+    setIsPaused(!isPaused);
+  };
+
+  return (
+    <div className="control-container">
+      <img src={cover} alt="Capa do Álbum" className="current"/>
+      <div className="buttons-container">
+        <ControlButton>
+          <img src={prev} alt="Voltar" className="button-icon"/>
+        </ControlButton>
+        <ControlButton>
+          <img src={stop} alt="Parar" className="button-icon"/>
+        </ControlButton>
+        <ControlButton>
+          <img 
+          src={isPaused ? pause : play} 
+          alt="Play/Pause" 
+          className="button-icon"
+          onClick={() => handlePlay()}
+          />
+        </ControlButton>
+        <ControlButton>
+          <img src={next} alt="Avançar" className="button-icon"/>
+        </ControlButton>
+
+      </div>
+    </div>
+  );
+};
+
+export default Controls;
